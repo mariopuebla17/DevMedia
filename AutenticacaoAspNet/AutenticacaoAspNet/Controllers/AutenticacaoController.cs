35e5976825bc0cb301f5a633102e86ca3e23e3a7
Mario Puebla Junior <mariopuebla17@outlook.com>
2018-11-19 16:13:30 -0200

    Até a aula 14

--- AutenticacaoAspNet/AutenticacaoAspNet/Controllers/AutenticacaoController.cs
+++ AutenticacaoAspNet/AutenticacaoAspNet/Controllers/AutenticacaoController.cs
@@ -1,10 +1,12 @@
-﻿using AutenticacaoAspNet.Models;
+﻿using AutenticacaoAspNet.Utils;
+using AutenticacaoAspNet.Models;
 using AutenticacaoAspNet.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Security.Claims;
 
 namespace AutenticacaoAspNet.Controllers
 {
@@ -26,17 +28,70 @@ namespace AutenticacaoAspNet.Controllers
                 return View(viewmodel);
             }
 
+            if (db.Usuarios.Count(u => u.Login == viewmodel.Login) > 0)
+            {
+                ModelState.AddModelError("Login", "Esse login já está em uso");
+                return View(viewmodel);
+            }
+
             Usuario novoUsuario = new Usuario
             {
                 Nome = viewmodel.Nome,
                 Login = viewmodel.Login,
-                Senha = viewmodel.Senha
+                Senha = Hash.GerarHash(viewmodel.Senha)
             };
 
             db.Usuarios.Add(novoUsuario);
             db.SaveChanges();
 
             return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Login(string ReturnUrl)
+        {
+            var viewmodel = new LoginViewModel
+            {
+                UrlRetorno = ReturnUrl
+            };
+
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel viewmodel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewmodel);
+            }
+
+            var usuario = db.Usuarios.FirstOrDefault(u => u.Login == viewmodel.Login);
+
+            if (usuario == null)
+            {
+                ModelState.AddModelError("Login", "Login incorreto");
+                return View(viewmodel);
+            }
+
+            if (usuario.Senha != Hash.GerarHash(viewmodel.Senha))
+            {
+                ModelState.AddModelError("Senha", "Senha incorreta");
+                return View(viewmodel);
+            }
+
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, usuario.Nome),
+                new Claim("Login", usuario.Login)
+            }, "ApplicationCookie");
+
+            Request.GetOwinContext().Authentication.SignIn(identity);
+
+            if (!String.IsNullOrWhiteSpace(viewmodel.UrlRetorno) || Url.IsLocalUrl(viewmodel.UrlRetorno))
+                return Redirect(viewmodel.UrlRetorno);
+            else
+                return RedirectToAction("Index", "Painel");
+
         }
     }
 }
---
+++ AutenticacaoAspNet/AutenticacaoAspNet/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutenticacaoAspNet.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UrlRetorno { get; set; }
+
+        [Required(ErrorMessage = "Informe o login")]
+        [MaxLength(50, ErrorMessage = "O login deve ter até 50 caracteres")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "A senha deve ter pelo menos 6 caracteres")]
+        public string Senha { get; set; }
+    }
+}
