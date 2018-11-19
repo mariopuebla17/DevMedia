9b54ff9ac57e9afbded98f4f4c0a39fc0212455c
Mario Puebla Junior <mariopuebla17@outlook.com>
2018-11-19 10:18:49 -0200

    Configuração inicial do curso. (Aulas 1, 2 e 3)

---
+++ PrimeirosPassosEntityFramework/Data/ProductDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(): base("Name=ProductDB")
+        {
+            Database.SetInitializer<ProductDbContext>(new CreateDatabaseIfNotExists<ProductDbContext>());
+        }
+
+        public DbSet<Produtos> Produtos { get; set; }
+        public DbSet<Loja> Lojas { get; set; }
+    }
+}
---
+++ PrimeirosPassosEntityFramework/Domain/Produtos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    [Table("Produto")]
+    public class Produtos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal valor { get; set; }
+    }
+}
